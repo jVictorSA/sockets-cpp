a7462c904c978a8431a9cf94beef2808400fbff2
Flaera <rafaelldsrock@gmail.com>
2022-06-04 20:54:30 -0300

    Improvements in chat conversation, but the bug in receiver function persist

--- cliente.cpp
+++ cliente.cpp
@@ -21,21 +21,59 @@ using namespace std;
 
 #define DEFAULT_BUFLEN 4096
 #define DEFAULT_PORT "27015"
+#define DEBUG if(1)
 
-int sender(SOCKET cliente){
+void JoinName(char *destination, char msg[], char *name){
+    int len_msg = strlen(msg);
+    int len_name = strlen(name);
+    //DEBUG{cout<<"dest:"<<destination<<", msg:"<<msg<<", name:"<<name<<endl;}
+    int i = 0;
+    for (i=0; i<len_name;++i){
+        destination[i] = name[i];
+        DEBUG{cout<<"i:"<<i<<destination<<endl;}
+    }
+    
+    destination[i] = ':';
+    destination[i+1] = ' ';
+    //DEBUG{cout<<"dest:"<<destination<<", msg:"<<msg<<", name:"<<name<<endl;}
+    int j = i+2;
+    int acc = 0;
+    //DEBUG{cout<<"STEP INTERMEDIARY"<<endl;}
+    for (j=i+2; j<len_name+len_msg+3; ++j){
+        //DEBUG{cout<<"dest:"<<destination<<", msg:"<<msg<<", name:"<<name<<endl;}
+        destination[j] = msg[acc];
+        acc+=1;
+    }
+    destination[j+1] = '\0';
+
+}
+
+int sender(SOCKET cliente, char *name, int *state){
     char recvbuf[DEFAULT_BUFLEN];
     int sentRes;
     int recvbuflen = DEFAULT_BUFLEN;
 
     while (true){
         string message;
+        cout << "Diga algo se quiser ou digite 1 para sair:"<<endl;
         getline(cin, message);
-        const int n = message.length();
-        char sendMes[n+1];
-        strcpy(sendMes, message.c_str());
+        //const int n = message.length();
+        char sendMes[DEFAULT_BUFLEN];
 
+        DEBUG{cout<<"first:"<<message[0]<<endl;}
+        if(message[0]=='1'){*state=2; return 2;}
+
+        DEBUG{cout << "STEP 5"<<endl;}
+        strcpy(sendMes, message.c_str());
+        DEBUG{cout << "sendMes:"<<sendMes<<endl;}  
+        DEBUG{cout << "STEP 6"<<endl;}
+        //Putting user name:
+        char sendMes2[DEFAULT_BUFLEN];
+        JoinName(sendMes2, sendMes, name);
+        DEBUG{cout << "STEP 7"<<endl;}  
+        
         // Send an initial buffer
-        sentRes = send(cliente, sendMes, (int)strlen(sendMes), 0);
+        sentRes = send(cliente, sendMes2, (int)strlen(sendMes), 0);
 
         if (sentRes == SOCKET_ERROR) {
             std::cout << "send failed with error: " << WSAGetLastError() << "\n";
@@ -60,7 +98,7 @@ int sender(SOCKET cliente){
     return 0;
 }
 
-int receiver(SOCKET cliente){
+int receiver(SOCKET cliente, int *state){
         
     char recvbuf[DEFAULT_BUFLEN];
     int recvRes;
@@ -70,13 +108,15 @@ int receiver(SOCKET cliente){
         memset(recvbuf, 0, DEFAULT_BUFLEN);
         
         recvRes = recv(cliente, recvbuf, recvbuflen, 0);
+        DEBUG{cout << "recv:"<<recvRes<<endl;}
         if ( recvRes > 0 ){
-            std::cout << "Bytes recebidos: " << recvRes << "\n";
-            std::cout << "Msg recebida: " << recvbuf << "\n\n";
+            DEBUG{std::cout << "Bytes recebidos: " << recvRes << "\n";}
+            std::cout << endl<<"Msg recebida: " << recvbuf << "\n\n";
         }
         else if ( recvRes == 0 ){
             std::cout << "Connection closed\n";
-            continue;
+            if (*state==2){return 2;}
+            //continue; //It's continue step the recvRes update
         }
     }
 
@@ -91,7 +131,14 @@ int __cdecl main(int argc, char **argv) {
                     *ptr = NULL,
                     hints;
     int iResult;
-    
+
+    //User name:
+    char name[DEFAULT_BUFLEN];
+    cout << "Digite seu nome:"<<endl;
+    cin.getline(name, DEFAULT_BUFLEN);
+    //getchar();
+    DEBUG{cout << "STEP 4"<<endl;}
+
     // Validate the parameters
     if (argc != 2) {
         std::cout << "usage: " << argv[0] << "server-name\n";
@@ -148,13 +195,25 @@ int __cdecl main(int argc, char **argv) {
         return 1;
     }
 
-    thread sendThread(sender, ConnectSocket);
-    thread recThread(receiver, ConnectSocket);
+    DEBUG{cout << "STEP 1 - initial of threads"<<endl;}
+    int *state = 0;
+    thread sendThread(sender, ConnectSocket, name, state);
+    thread recThread(receiver, ConnectSocket, state);
 
+    DEBUG{cout << "STEP 2 - into in threads, state:"<<state<<endl;}
     // Receive until the peer closes the connection
-    
+    if (*state==2){    
+        // cleanup
+        closesocket(ConnectSocket);
+        WSACleanup();
+
+        return 0;
+    }
+
     sendThread.join();
     recThread.join();    
+    
+    DEBUG{cout << "STEP 3 - Dead of program"<<endl;}
 
     // cleanup
     closesocket(ConnectSocket);
